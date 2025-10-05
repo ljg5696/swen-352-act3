142cd2bce65ea869c66a9a65ed36aed93ef1692a
Ryanerror <ryangarvin19@gmail.com>
2025-10-05 19:33:35 -0400

    simplebankaccount : 1a : construct a bank account that starts at float  0.0 : create passing code

--- src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
@@ -25,8 +25,21 @@ public class SimpleBankAccount {
     public SimpleBankAccount() {
         this.balance = 0.00f;
     }
+    
 
     public float getBalance() {
        return balance;
+    }
+
+    public void withdraw(float amount){
+
+    }
+
+    public void deposit(float amount){
+        
+    }
+
+    public void isAccountEmpty(){
+        
     }
 }
